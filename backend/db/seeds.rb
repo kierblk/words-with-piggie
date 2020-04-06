c6d99055f7f2ce3b13bd9ff33076a4378af43c6e
saramccombs <theSaraMcCombs@gmail.com>
2020-04-06 19:02:06 -0400

    Created seed data.

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create([
+  {
+    title: "Colors", 
+    description: "Flash cards that will teach all colors of the rainbow."
+  },
+  {
+    title: "Sight Words",
+    description: "Flash cards that target sight words."
+  }
+])
+
+Card.create([
+  {
+    title: "Red", 
+    description: "A red ball.", 
+    image: "image url here", 
+    category: Category.first
+  },
+  {
+    title: "Orange", 
+    description: "A orange ball.", 
+    image: "image url here", 
+    category: Category.first
+  },
+  {
+    title: "Yellow", 
+    description: "A yellow ball.", 
+    image: "image url here", 
+    category: Category.first
+  },
+  {
+    title: "Green", 
+    description: "A green ball.", 
+    image: "image url here", 
+    category: Category.first
+  },
+  {
+    title: "Blue", 
+    description: "A blue ball.", 
+    image: "image url here", 
+    category: Category.first
+  },
+  {
+    title: "Indigo", 
+    description: "A indigo ball.", 
+    image: "image url here", 
+    category: Category.first
+  },
+  {
+    title: "Violet", 
+    description: "A violet ball.", 
+    image: "image url here", 
+    category: Category.first
+  }
+])
