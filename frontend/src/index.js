c0ae47057aa9f6260c87c869b87098aa480880c3
saramccombs <theSaraMcCombs@gmail.com>
2020-04-09 21:34:28 -0400

    Fleshing out a makeCards function.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,41 @@
 document.addEventListener("DOMContentLoaded", init)
 
 function init(){
-  // console.log("Tea. Earl Grey. Hot.")
+  console.log("INIT FN: Tea. Earl Grey. Hot.")
+  fetchCards()
+}
 
+function fetchCards() {
+  console.log("FETCHCARDS FN: Make it so.")
   const backendURL = 'localhost:3000'
 
   fetch(`http://${backendURL}/cards`)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(cardsJSON => cardsJSON.forEach(card => makeCards(card)))
+}
+
+function makeCards(card) {
+
+  const mainDiv = document.querySelector('.main')
+
+  const newCardDiv = document.createElement('div')
+  newCardDiv.setAttribute('class', 'card text-center card-display-ib')
+
+  newCardDiv.innerHTML = `
+    <div class="card-body">
+      <h5 class="card-title">${card.title}</h5>
+      <img id="card-image" src="${card.image}"><br />
+      <p class="card-text">${card.description}</p>
+      <a href="#" class="btn btn-info"><i class="far fa-check-circle fa-1x"></i></a>
+      <a href="#" class="btn btn-primary"><i class="far fa-times-circle fa-1x"></i></a>
+    </div>
+    <div class="card-footer text-muted">
+    <i class="fas fa-edit"></i> <i class="far fa-trash-alt "></i>
+    </div>
+  `
+
+  mainDiv.appendChild(newCardDiv)
+
+  console.log(newCardDiv)
+
 }
